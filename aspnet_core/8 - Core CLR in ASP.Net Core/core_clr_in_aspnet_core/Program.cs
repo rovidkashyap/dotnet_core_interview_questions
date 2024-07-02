de6727ae633018faf8c2980372d4a1a7b9f66a21
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-02 14:30:59 +0530

    Question 8 - Core CLR in ASP.Net Core

---
+++ aspnet_core/8 - Core CLR in ASP.Net Core/core_clr_in_aspnet_core/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+
+    Core CLR(Core Common Language Runtime) is the runtime used by .NET Core applications. It is a 
+    cross-platform, high-performance, lightweight, and modular runtime for executing .NET 
+    applications. Core CLR is a key component of the .NET Core ecosystem, providing the essential 
+    services needed to run .NET applications, including memory management, garbage collection, 
+    assembly loading, security, exception handling, and interoperability with native code.
+
+    KEY FEATURES OF CORE CLR
+
+    1. Cross-Platform: Core CLR is designed to run on multiple platforms, including Windows, macOS, 
+                       and Linux, enabling .NET Core applications to be developed and deployed across 
+                       different operating systems.
+
+    2. High Performance: Core CLR is optimized for high performance, making it suitable for a wide 
+                         range of application types, from cloud services to console applications. It 
+                         includes a Just-In-Time (JIT) compiler that compiles Intermediate Language 
+                         (IL) code to native machine code at runtime.
+
+    3. Lightweight and Modular: Core CLR is lightweight and modular, allowing developers to include 
+                                only the necessary components for their applications. This modularity 
+                                reduces the overall footprint of applications and improves deployment 
+                                flexibility.
+
+    4. Open Source: Core CLR is open source, hosted on GitHub, and developed by the .NET community and 
+                    Microsoft. This openness encourages community contributions and transparency in 
+                    development.
+
+    5. Managed Execution: Core CLR provides managed execution of .NET code, ensuring safety, security, 
+                          and portability across different environments. It includes features like 
+                          type safety, automatic memory management, and exception handling.
+
+    6. Garbage Collection: Core CLR includes a sophisticated garbage collector (GC) that automatically 
+                           manages memory allocation and reclamation, helping to prevent memory leaks 
+                           and optimize application performance.
+
+    7. Interoperability: Core CLR supports interoperability with native code through Platform 
+                         Invocation Services (P/Invoke) and the use of COM components. This allows 
+                         .NET applications to interact with existing native libraries and components.
+
+*/
