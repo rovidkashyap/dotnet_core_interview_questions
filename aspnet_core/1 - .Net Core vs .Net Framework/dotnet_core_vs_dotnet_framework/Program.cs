2a2bf0ee936474afd34b28bdd4b0696faf06ea75
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-02 12:55:41 +0530

    Question 1 - .Net Core vs .Net

--- aspnet_core/1 - .Net Core vs .Net Framework/dotnet_core_vs_dotnet_framework/Program.cs
+++ aspnet_core/1 - .Net Core vs .Net Framework/dotnet_core_vs_dotnet_framework/Program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+/*
+ 
+    The distinction between .NET Core and .NET involves several factors, including their origins, 
+    supported platforms, and purposes. Here's a breakdown of the differences:
+    
+    1. .Net Framework Introduce in 2002 for windows       2. .Net Core introduce in 2016 for cross-
+    platform only and primarly for building               platform (windows, macOS, linux) and designed
+    windows applications including desktop and            for cross-platform app and cloud-based
+    web applications                                      applications.
 
+    2. Mostly Proprietary with some parts open source     2. Completely open source under .net foundation.
+
+    3. .Net Framework is the older, Windows-only frame    3. .Net Core is the newer, cross-platform
+    work suitable for legacy applications.                designed for modern, scalable and high perf
+                                                          ormance applications.
+ 
+ */                                                          
