3ef0eced534ef03bf40374434f519f1a08842ba2
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-03 15:29:32 +0530

    Question 19 - IHostedService in ASP.Net Core

---
+++ aspnet_core/19 - IHostedService in ASP.Net Core/ihostedservice_in_aspnet_core/MyBackgrounService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ihostedservice_in_aspnet_core
+{
+    public class MyBackgrounService : IHostedService, IDisposable
+    {
+        private readonly ILogger<MyBackgrounService> _logger;
+        private Timer _timer;
+
+        public MyBackgrounService(ILogger<MyBackgrounService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("MyBackgroundService is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("MyBackgroundService is working.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("MyBackgroundService is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ aspnet_core/19 - IHostedService in ASP.Net Core/ihostedservice_in_aspnet_core/README.txt
@@ -0,0 +1,19 @@
+﻿
+In ASP.Net Core, `IHostedService` is an interface that allows you to run background tasks or services
+on application startup and shutdown. It provides a way to execute long-running background operations
+independently of the main application thread, it suitable for scenarios such as background tasks, 
+timers, or periodic operations.
+
+1. `IHostedService` allows you to define background tasks that run asynchronously when the application
+	starts.
+
+2. Implement the `StartAsync(cancellationToken)` and `StopAsync(cancellationToken)` methods, which are
+   invoked by the application's lifetime management.
+
+3. Can be injected with dependencies using the ASP.Net Core dependency injection mechanism.
+
+4. `StartAsync(CancellationToken)` is called when the application starts, allowing initialization of
+	the hosted service.
+
+5. `StopAsync(CancellationToken)` is called when the application is shutting down, allowing cleanup
+	tasks to be performed.
