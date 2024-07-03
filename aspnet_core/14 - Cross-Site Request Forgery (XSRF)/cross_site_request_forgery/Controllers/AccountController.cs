1036c3938f0332b54c7a193f483d6d36c239deb9
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-03 12:48:26 +0530

    Question 14 - Cross-Site Request Forgery in ASP.Net Core

---
+++ aspnet_core/14 - Cross-Site Request Forgery (XSRF)/cross_site_request_forgery/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cross_site_request_forgery.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string username, string password)
+        {
+            // Login logic
+            return View();
+        }
+    }
+}
